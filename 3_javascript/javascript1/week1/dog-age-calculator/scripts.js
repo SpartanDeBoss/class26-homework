c1a5338ef0c77cdf5ee508787d2151953b80d4dc
Aaron Moses <pastures77@gmail.com>
2023-08-28 18:52:03 +0200

    added scripts.js

---
+++ 3_javascript/javascript1/week1/dog-age-calculator/scripts.js
@@ -0,0 +1,9 @@
+//A dog age calculator
+let dogYearOfBirth;
+let dogYearFuture;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears;
+if (shouldShowResultInDogYears = true) {
+  console.log("Your dog will be " + 70  + " dog years old in " + 2027)
+}
+
