e1e8b5b6cd4656d13f1c6ca6964ef2e9ef353a54
Aaron Moses <pastures77@gmail.com>
2023-09-01 08:50:57 +0200

    modify feedback

--- 3_javascript/javascript1/week1/startup-name-generator/scripts.js
+++ 3_javascript/javascript1/week1/startup-name-generator/scripts.js
@@ -1,34 +1,37 @@
 //startup-name-generator
 const firstWords = [
-  "Wecan",
-  "Undefined",
-  "Answers",
-  "Datany",
-  "Primeloop",
-  "Duany",
-  "Anyx",
-  "Virtan",
-  "Artyfy",
-  "Luvany",
+  'Wecan',
+  'Undefined',
+  'Answers',
+  'Datany',
+  'Primeloop',
+  'Duany',
+  'Anyx',
+  'Virtan',
+  'Artyfy',
+  'Luvany',
 ];
 
 const secondWords = [
-  "Onyx",
-  "Eligo",
-  "Haify",
-  "Motion",
-  "Chosen",
-  "Answered",
-  "Blitzly",
-  "Voxify",
-  "BlueSwifty",
-  "LevelLeap",
+  'Onyx',
+  'Eligo',
+  'Haify',
+  'Motion',
+  'Chosen',
+  'Answered',
+  'Blitzly',
+  'Voxify',
+  'BlueSwifty',
+  'LevelLeap',
 ];
-const randomFirstWord =
-  firstWords[Math.floor(Math.random() * firstWords.length)];
-const randomSecondWord =
-  secondWords[Math.floor(Math.random() * secondWords.length)];
+
+const randomNumber = (array) => Math.floor(Math.random() * array.length);
+
+const randomFirstWord = firstWords[randomNumber(firstWords)];
+const randomSecondWord = secondWords[randomNumber(secondWords)];
+
 const startupName = `The startup: ${randomFirstWord} ${randomSecondWord} contains ${
   randomFirstWord.length + randomSecondWord.length
 } characters`;
+
 console.log(startupName);
